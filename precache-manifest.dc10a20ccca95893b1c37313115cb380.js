64a972607afa92e2abfb3e6e7440008e95589202
Adam <adampugh@zoho.com>
2019-03-18 23:06:38 +0000

    Updates

---
+++ precache-manifest.dc10a20ccca95893b1c37313115cb380.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "7311c6cc90c92181a945db9a4f27ad41",
+    "url": "/adampugh/pgoal/static/media/bulbasaur.7311c6cc.png"
+  },
+  {
+    "revision": "51bb66b5318b1cf6199c",
+    "url": "/adampugh/pgoal/static/css/main.076378a2.chunk.css"
+  },
+  {
+    "revision": "ed0dad85c81ec46f7969",
+    "url": "/adampugh/pgoal/static/js/runtime~main.ed0dad85.js"
+  },
+  {
+    "revision": "54c618cacf93da0fd7ce52b9c4f1a2a6",
+    "url": "/adampugh/pgoal/static/media/bg1.54c618ca.png"
+  },
+  {
+    "revision": "460b80f650cd8ebe8f69",
+    "url": "/adampugh/pgoal/static/js/2.460b80f6.chunk.js"
+  },
+  {
+    "revision": "387b5a7db6417f68fdbcbf9befd47069",
+    "url": "/adampugh/pgoal/static/media/logo.387b5a7d.png"
+  },
+  {
+    "revision": "0dfe313b500dde81f2a9780f7d2c6879",
+    "url": "/adampugh/pgoal/static/media/pikachu.0dfe313b.png"
+  },
+  {
+    "revision": "5aca14c062cc5cfead0230cefe902559",
+    "url": "/adampugh/pgoal/static/media/marill.5aca14c0.png"
+  },
+  {
+    "revision": "425dffb373b14650acfa20456b58433d",
+    "url": "/adampugh/pgoal/static/media/squirtle.425dffb3.png"
+  },
+  {
+    "revision": "e1988823bbd152d0b412acb6ca1d895d",
+    "url": "/adampugh/pgoal/static/media/eevee.e1988823.png"
+  },
+  {
+    "revision": "a28c3748aa024028e70ef48d41994040",
+    "url": "/adampugh/pgoal/static/media/gengar.a28c3748.png"
+  },
+  {
+    "revision": "c6014b14c0b465266e42b2e1882b129d",
+    "url": "/adampugh/pgoal/static/media/umbreon.c6014b14.png"
+  },
+  {
+    "revision": "c46f6580b506f828a9851ed521106ed7",
+    "url": "/adampugh/pgoal/static/media/jigglypuff.c46f6580.png"
+  },
+  {
+    "revision": "af752d343e9cd1db572afeb3b7edf416",
+    "url": "/adampugh/pgoal/static/media/oddish.af752d34.png"
+  },
+  {
+    "revision": "51bb66b5318b1cf6199c",
+    "url": "/adampugh/pgoal/static/js/main.51bb66b5.chunk.js"
+  },
+  {
+    "revision": "0a8d1bd9add966e9cbc4694289c0eaf1",
+    "url": "/adampugh/pgoal/static/media/egg.0a8d1bd9.gif"
+  },
+  {
+    "revision": "9b23f9bb6359ca3c91b837ad94675cd2",
+    "url": "/adampugh/pgoal/static/media/mockup2.9b23f9bb.png"
+  },
+  {
+    "revision": "d6330a9d746c7a66f2eb27cec182ea43",
+    "url": "/adampugh/pgoal/static/media/dragonite.d6330a9d.png"
+  },
+  {
+    "revision": "a86026d64716d520f6ab4c4c0ba5e699",
+    "url": "/adampugh/pgoal/static/media/team2.a86026d6.png"
+  },
+  {
+    "revision": "4bd33b1c680851b8c94856b0ef89f18e",
+    "url": "/adampugh/pgoal/static/media/evolution.4bd33b1c.png"
+  },
+  {
+    "revision": "0bb7e351d8b7d0091ba2d42b244f8dd6",
+    "url": "/adampugh/pgoal/static/media/banner.0bb7e351.png"
+  },
+  {
+    "revision": "e0b146446bc6d6f3aeff537c5f591390",
+    "url": "/adampugh/pgoal/static/media/giratina.e0b14644.png"
+  },
+  {
+    "revision": "9c8cb2f1d7628d305945696ddab18b19",
+    "url": "/adampugh/pgoal/static/media/mew.9c8cb2f1.png"
+  },
+  {
+    "revision": "2271c7ca40eba28184c6d21bcb387e6b",
+    "url": "/adampugh/pgoal/static/media/raichu.2271c7ca.png"
+  },
+  {
+    "revision": "e2548b498ca94b030485a99b6c533be8",
+    "url": "/adampugh/pgoal/static/media/pikachuloading.e2548b49.gif"
+  },
+  {
+    "revision": "54c618cacf93da0fd7ce52b9c4f1a2a6",
+    "url": "/adampugh/pgoal/static/media/homepagebg.54c618ca.png"
+  },
+  {
+    "revision": "460b80f650cd8ebe8f69",
+    "url": "/adampugh/pgoal/static/css/2.e18ea423.chunk.css"
+  },
+  {
+    "revision": "51d8ab45b750568ea2a8201ffc5531e2",
+    "url": "/adampugh/pgoal/index.html"
+  }
+];
